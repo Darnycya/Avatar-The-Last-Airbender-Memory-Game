4852b0ff5b809c7047df0f61cfd05b2bafefd025
Darnycya Smith <darnycyasmith@DarnycymithsMBP.lan1>
2020-08-24 14:15:52 -0400

    created game board

--- game/src/App.js
+++ game/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Board from './components/Board/Board'
+import Aang from './Assets/Images/Aang2.jpg'
+import Appa from './Assets/Images/Appa.png'
+import Katara from './Assets/Images/Katara.jpeg'
+import PrinceZuko from './Assets/Images/Prince_Zuko.jpg'
+import Sokka from './Assets/Images/Sokka.png'
+import Toph from './Assets/Images/Toph.jpg'
+
 
 function App() {
+  const cards = buildCards()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board cards={cards} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+function buildCards() {
+  let id = 0
+  const images = {Aang, Appa, Katara, PrinceZuko, Sokka, Toph}
+  const cards = Object.keys(images).reduce((result, item) => {
+    const getCard = () => ({
+      id: id++,
+      type: item,
+      backImg,
+      frontImg: images[item],
+      flipped: false,
+    })
+    return [...result, getCard(), getCard()]
+  }, [])
+  return suffle(cards)
+}
+
+function suffle(arr) {
+  let len = arr.length
+  for (let i = 0; i < len; i++) {
+    let randomIdx = Math.floor(Math.random() * len)
+    let copyCurrent = {...arr[i]}
+    let copyRandom = {...arr[randomIdx]}
+    arr[i] = copyRandom
+    arr[randomIdx] = copyCurrent
+  }
+  return arr
+}
