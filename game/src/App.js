7e80535327acf02def24877b2b75083e36de78d6
Darnycya Smith <darnycyasmith@DarnycymithsMBP.lan1>
2020-08-24 19:10:01 -0400

    finished basic

--- game/src/App.js
+++ game/src/App.js
@@ -1,48 +1,21 @@
-import React, {useState} from 'react'
-import Board from './components/Board/Board'
-import Aang from './Assets/Images/Aang2.jpg'
-import Appa from './Assets/Images/Appa.png'
-import Katara from './Assets/Images/Katara.jpeg'
-import PrinceZuko from './Assets/Images/Prince_Zuko.jpg'
-import Sokka from './Assets/Images/Sokka.png'
-import Toph from './Assets/Images/Toph.jpg'
+import React from 'react'
+import { Route } from "react-router-dom"
+import HomePage from "./Components/HomePage/HomePage"
+import Main from "./Components/Main"
+
 
 
 function App() {
-  const cards = buildCards()
   return (
-    <div className="App">
-      <Board cards={cards} />
-    </div>
-  )
+    <>
+      <Route path="/" exact>
+        <HomePage />
+      </Route>
+      <Route path="/gamepage">
+        <Main />
+      </Route>
+    </>
+  );
 }
 
-export default App
-
-function buildCards() {
-  let id = 0
-  const images = {Aang, Appa, Katara, PrinceZuko, Sokka, Toph}
-  const cards = Object.keys(images).reduce((result, item) => {
-    const getCard = () => ({
-      id: id++,
-      type: item,
-      backImg,
-      frontImg: images[item],
-      flipped: false,
-    })
-    return [...result, getCard(), getCard()]
-  }, [])
-  return suffle(cards)
-}
-
-function suffle(arr) {
-  let len = arr.length
-  for (let i = 0; i < len; i++) {
-    let randomIdx = Math.floor(Math.random() * len)
-    let copyCurrent = {...arr[i]}
-    let copyRandom = {...arr[randomIdx]}
-    arr[i] = copyRandom
-    arr[randomIdx] = copyCurrent
-  }
-  return arr
-}
+export default App;
---
+++ game/src/Components/HomePage/HomePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './HomePage.css'
+
+
+class HomePage extends Component {
+ 
+
+  render() {
+    return (
+      <div className="main">
+        <h1>Welcome to Avatar: The Last Airbender Memory Game</h1>
+        <p> Objective: Click a card to flip it over and try to find it's match. Once you have found all pairs you have won the game. Press Enter to begin.</p>
+        
+        <Link to="/gamepage"><button>Enter</button></Link>
+      </div>
+    )
+  }
+}
+export default HomePage;
