f9d8389a1caa427ed1ecb432fb493abf0b10848a
Darnycya Smith <darnycyasmith@DarnycymithsMBP.lan1>
2020-08-25 17:50:38 -0400

    first commit

---
+++ build/precache-manifest.69deb1246b96e214c67e55d9b16f1fac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a1e5f637bf810975d4d73495aa2632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7263cbe2c18936e682ae",
+    "url": "/static/css/main.0d2f4c8e.chunk.css"
+  },
+  {
+    "revision": "b0ea7ad1dceba9587dd3",
+    "url": "/static/js/2.7f2628ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f2628ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7263cbe2c18936e682ae",
+    "url": "/static/js/main.734d19c6.chunk.js"
+  },
+  {
+    "revision": "4c151dd322d1d8c7868f",
+    "url": "/static/js/runtime-main.2ef96d68.js"
+  },
+  {
+    "revision": "8445144e118a6a6d72f52cb6afc7b452",
+    "url": "/static/media/Aang.8445144e.jpg"
+  },
+  {
+    "revision": "22d0c65f76576f38812fd990b1a874c5",
+    "url": "/static/media/Appa.22d0c65f.png"
+  },
+  {
+    "revision": "d7cab81512488467d79d14515267fba5",
+    "url": "/static/media/Katara.d7cab815.jpeg"
+  },
+  {
+    "revision": "022283b3b26cf243d023315ffc034a4f",
+    "url": "/static/media/Logo.022283b3.jpg"
+  },
+  {
+    "revision": "b77e491561248ffb1fc3300435495dab",
+    "url": "/static/media/Prince_Zuko.b77e4915.jpg"
+  },
+  {
+    "revision": "95d52468f2d0131bc5c617c1b82c63a9",
+    "url": "/static/media/Sokka.95d52468.png"
+  },
+  {
+    "revision": "5d90638abe4cd120239b2a8ba26815d4",
+    "url": "/static/media/Toph.5d90638a.jpg"
+  }
+]);
---
+++ src/Components/Main.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Board from '../Components/Board/Board'
+import Appa from '../Assets/Images/Appa.png'
+import Katara from '../Assets/Images/Katara.jpeg'
+import PrinceZuko from '../Assets/Images/Prince_Zuko.jpg'
+import Sokka from '../Assets/Images/Sokka.png'
+import Toph from '../Assets/Images/Toph.jpg'
+import Aang from '../Assets/Images/Aang.jpg'
+import backImg from '../Assets/Images/Logo.jpg'
+
+
+function Main() {
+  const cards = buildCards()
+  return (
+    <div className="App">
+      <Board cards={cards} />
+    </div>
+  )
+}
+
+export default Main
+
+function buildCards() {
+  let id = 0
+  const images = {Aang, Appa, Katara, PrinceZuko, Sokka, Toph}
+  const cards = Object.keys(images).reduce((result, item) => {
+    const getCard = () => ({
+      id: id++,
+      type: item,
+      backImg,
+      frontImg: images[item],
+      flipped: false,
+    })
+    return [...result, getCard(), getCard()]
+  }, [])
+  return suffle(cards)
+}
+
+function suffle(arr) {
+  let len = arr.length
+  for (let i = 0; i < len; i++) {
+    let randomIdx = Math.floor(Math.random() * len)
+    let copyCurrent = {...arr[i]}
+    let copyRandom = {...arr[randomIdx]}
+    arr[i] = copyRandom
+    arr[randomIdx] = copyCurrent
+  }
+  return arr
+}
