2b76d5536980bb32bea6efbe3afa6090e83ab9a9
Darnycya <Darnycya@gmail.com>
2021-03-05 14:45:30 -0500

    installed bootstrap

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,38 @@
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <img src="https://res.cloudinary.com/darnycya/image/upload/c_scale,w_450/v1614972962/YouWon_swaqtv.png"/>
+      </Modal.Body>
+     
+    </Modal>
+  );
+}
+
+function App() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
+
+render(<App />);
