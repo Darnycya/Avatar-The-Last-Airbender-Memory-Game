25778bf03db8c1eb1abcf6f50d3823996c76236c
Darnycya <Darnycya@gmail.com>
2021-10-19 10:29:39 -0400

    fixed homepage layout for tablet

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -11,6 +11,7 @@ export default function HomePage() {
       <div className="main">
         <h1>AVATAR: THE LAST AIRBENDER MEMORY GAME</h1>
         <div className="objective">
+        <p><i>To be played on Desktop or Tablet.</i></p>
         <p> Objective: Click a card to flip it over and click another to try to find its match. Once you have found all pairs you have won the game. Use the timer to keep track of how fast you can complete the game!</p>
         </div>
         <Link to="/gamepage"><button className="homepage-button"><p className="button-font">Enter</p></button></Link>
