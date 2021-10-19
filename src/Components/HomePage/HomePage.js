6d79cdf14821dda6b019c64e949e429517f9d676
Darnycya <Darnycya@gmail.com>
2021-10-19 08:57:36 -0400

    adjusted border and font

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -10,8 +10,9 @@ export default function HomePage() {
     return (
       <div className="main">
         <h1>AVATAR: THE LAST AIRBENDER MEMORY GAME</h1>
+        <div className="objective">
         <p> Objective: Click a card to flip it over and click another to try to find its match. Once you have found all pairs you have won the game. Use the timer to keep track of how fast you can complete the game!</p>
-        
+        </div>
         <Link to="/gamepage"><button className="homepage-button"><p className="button-font">Enter</p></button></Link>
   
           
